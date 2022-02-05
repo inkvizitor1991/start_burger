04e41e9827c919c1a54aad9fc601b3b9fcb209ce
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2022-02-05 15:46:02 +0300

    Made the code flatter

--- restaurateur/restaurants_orders.py
+++ restaurateur/restaurants_orders.py
@@ -9,19 +9,20 @@ def add_restaurants_orders(restaurants, orders):
             order_products.append(order_product.product.name)
         restaurant_matches_count = {}
         for restaurant in restaurants:
-            if restaurant.product.name in order_products:
-                matches_number = restaurant_matches_count.get(
-                    restaurant.restaurant.name, 0)
-                restaurant_matches_count[
-                    restaurant.restaurant.name] = matches_number + 1
-                if restaurant_matches_count[restaurant.restaurant.name] >= len(order_products):
-                    order.restaurant = restaurant.restaurant
-                    order.save()
-                    calculated_distance = calculate_distance(
-                        restaurant.restaurant, order.address)
-
-                    orders_restaurants[order.id] = sorted(
-                        orders_restaurants.get(order.id, []) + [
-                            (restaurant.restaurant.name, calculated_distance)],
-                        key=lambda restaurant: restaurant[1])
+            if not restaurant.product.name in order_products:
+                continue
+            matches_number = restaurant_matches_count.get(
+                restaurant.restaurant.name, 0)
+            restaurant_matches_count[
+                restaurant.restaurant.name] = matches_number + 1
+            if not restaurant_matches_count[restaurant.restaurant.name] >= len(order_products):
+                continue
+            order.restaurant = restaurant.restaurant
+            order.save()
+            calculated_distance = calculate_distance(
+                restaurant.restaurant, order.address)
+            orders_restaurants[order.id] = sorted(
+                orders_restaurants.get(order.id, []) + [
+                    (restaurant.restaurant.name, calculated_distance)],
+                key=lambda restaurant: restaurant[1])
     return orders_restaurants
